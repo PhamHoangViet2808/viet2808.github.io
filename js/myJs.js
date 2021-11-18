d084d2108de05f6dbe50e7264da67ca97619db65
PhamHoangViet2808 <81349228+PhamHoangViet2808@users.noreply.github.com>
2021-11-18 13:24:32 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -35,9 +35,9 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/hinh1.jpg",
-      imageWidth: 300,
-      imageHeight: 300,
+      imageUrl: "img/hinh3.jpg",
+      imageWidth: 500,
+      imageHeight: 500,
       background: '#fff url("img/hinh2.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
